1d140d6b0f8b4989d09564ec778f800c6f941325
Javad Hamidi <19621911+javadhamidi@users.noreply.github.com>
2020-10-03 20:57:45 +0930

    ☀️ and 🌙 mode can now be toggled

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,35 @@
-
 let mode = window.matchMedia('(prefers-color-scheme: dark)');
+let stored_theme = localStorage.getItem('theme');
 
-if (mode.matches) {
-    document.body.classList.add('dark');
+let indicator = document.body.getElementsByClassName("dark-mode-toggle")[0].children[0].src;
+
+if (!(stored_theme == '' || stored_theme == undefined)) {
+    if (stored_theme == 'dark') {
+        document.body.classList.add('dark');
+    }
+    else {
+        document.body.getElementsByClassName("dark-mode-toggle")[0].children[0].src = indicator.replace("light", "dark");
+    }
 }
+else if (mode.matches) {
+    document.body.classList.add('dark');
+    document.body.getElementsByClassName("dark-mode-toggle")[0].children[0].src = indicator.replace("dark", "light");
+}
+else { document.body.getElementsByClassName("dark-mode-toggle")[0].children[0].src = indicator.replace("light", "dark"); }
 
-
-let indicator;
 
 function toggleDarkMode() {
     indicator = document.body.getElementsByClassName("dark-mode-toggle")[0].children[0].src;
 
     document.body.classList.toggle('dark');
 
-    if (indicator.includes("light")) {
-        document.body.getElementsByClassName("dark-mode-toggle")[0].children[0].src = indicator.replace("light", "dark");
-    } else {
+    if (indicator.includes("dark")) {
         document.body.getElementsByClassName("dark-mode-toggle")[0].children[0].src = indicator.replace("dark", "light");
+        localStorage.setItem("theme", "dark");
+    } else {
+        document.body.getElementsByClassName("dark-mode-toggle")[0].children[0].src = indicator.replace("light", "dark");
+        localStorage.setItem("theme", "light");
     }
 }
-
 
 document.body.getElementsByClassName("dark-mode-toggle")[0].addEventListener('click', toggleDarkMode);
